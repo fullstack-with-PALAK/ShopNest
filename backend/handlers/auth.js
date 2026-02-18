eea8abb55fe377eab2b3f7b8c262f6d6e11f9cf6
fullstack-with-PALAK <behlpari4@gmail.com>
2026-02-19 01:16:32 +0530

    Replace bcrypt with bcryptjs for serverless compatibility

--- backend/handlers/auth.js
+++ backend/handlers/auth.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../schemas/users');
 const { generateToken } = require('./jwts');
 const { isDemoMode } = require('../mockData');
