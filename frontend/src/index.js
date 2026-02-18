f9826fb38fb934d353a2cef2cbc453af8c17e8fd
fullstack-with-PALAK <behlpari4@gmail.com>
2026-02-18 23:26:32 +0530

    Wrap app with SearchProvider context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HashRouter } from 'react-router';
+import { SearchProvider } from './SearchContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <HashRouter>
         <React.StrictMode>
-            <div className="min-h-screen w-full overflow-x-hidden overflow-y-auto font-sans text-black bg-white">
-                <App />
-            </div>
+            <SearchProvider>
+                <div className="min-h-screen w-full overflow-x-hidden overflow-y-auto font-sans text-black bg-white">
+                    <App />
+                </div>
+            </SearchProvider>
         </React.StrictMode>
     </HashRouter>
 );
