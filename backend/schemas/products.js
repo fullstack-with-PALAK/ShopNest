b4985f533e8f38795724db581c1438a9b058dadf
fullstack-with-PALAK <behlpari4@gmail.com>
2026-02-18 23:01:45 +0530

    Add Product schema for inventory management

---
+++ backend/schemas/products.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = {
+    productModel: mongoose.model('Product', productSchema),
+    productSchema: productSchema
+};
