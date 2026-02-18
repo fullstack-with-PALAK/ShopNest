8cf6e03ce943b05afc70e91a96a5dd0ab75a62d1
fullstack-with-PALAK <behlpari4@gmail.com>
2026-02-18 23:00:43 +0530

    Integrate MongoDB connection in server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const connect = require('./connect');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,15 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Database connection
+connect('shopnest')
+    .then(() => {
+        console.log('Connected to MongoDB successfully');
+    })
+    .catch((err) => {
+        console.log('MongoDB connection error:', err);
+    });
 
 // Basic route
 app.get('/', (req, res) => {
